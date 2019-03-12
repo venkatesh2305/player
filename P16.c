2a6bec683715b19165a9d901e6d1a921b886a089
venkatesh2305 <47910235+venkatesh2305@users.noreply.github.com>
2019-03-12 08:05:45 +0530

    Create P16.c

---
+++ P16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() 
+{
+	int n,i,j,count=0;
+	int a[100];
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d ",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(a[i]==a[j] && i!=j)
+			{
+				count=count+1;	
+			}
+
+		}
+		if(count==0)
+	{
+		printf("%d",a[i]);
+	}
+	}
+	return 0;
+}
